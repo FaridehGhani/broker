c1da4d99ee2994a66ca10622ecf6519c75efd813
FaridehGhani <faridehghani98@gmail.com>
2022-07-08 12:52:27 +0430

    add main

---
+++ main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"time"
+
+	zmq "github.com/pebbe/zmq4"
+
+	"github.com/FaridehGhani/broker/broker"
+	"github.com/FaridehGhani/broker/destination"
+	"github.com/FaridehGhani/broker/source"
+)
+
+func main() {
+	go source.Publish()
+	go destination.Subscribe()
+	go broker.Listen()
+
+	time.Sleep(100 * time.Millisecond)
+
+	subscriber, _ := zmq.NewSocket(zmq.XSUB)
+	subscriber.Connect("tcp://localhost:6000")
+	publisher, _ := zmq.NewSocket(zmq.XPUB)
+	publisher.Bind("tcp://*:6001")
+	listener, _ := zmq.NewSocket(zmq.PAIR)
+	listener.Connect("inproc://pipe")
+	zmq.Proxy(subscriber, publisher, listener)
+}
